b93f9b58301c29fbe394cd2ef96cf68353cfba8e
peaceiris <30958501+peaceiris@users.noreply.github.com>
2019-04-13 07:14:17 +0900

    add: netlify.toml and netlify.sh

---
+++ netlify.sh
@@ -0,0 +1,9 @@
+rm -rf hugoDocs
+mkdir .ja
+mv * .ja/
+git clone --depth=1 -b japanese https://github.com/hugojapan/hugoDocs.git
+cd hugoDocs
+rm -rf ./content/ja
+mv ../.ja ./content/ja
+hugo --gc --minify --baseURL 'https://hugodocsja.netlify.com/'
+echo -e 'User-agent: *\nDisallow: /' > public/robots.txt
